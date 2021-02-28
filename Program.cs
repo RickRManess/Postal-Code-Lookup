da97dab8692cbcbc83c9209c0359f4763a97b844
Richard Maness <rickrmaness@gmail.com>
2021-02-28 13:42:57 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Postal_Code_Lookup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable created to read postal code text file
+            var myLines = File.ReadLines(@"C:\Users\rrman\Downloads\US.zip");
+
+
+            //Introduction Text
+            Console.WriteLine("Please enter zip code");
+            var zipCode = (Console.ReadLine());
+            
+          
+            //foreach loop to find zipcode and return line
+            foreach (var line in myLines)
+            {
+                if (line.Contains(zipCode))
+                    {
+                    Console.WriteLine(line);
+                }
+                   
+
+                
+            }
+
+
+
+
+
+        }
+    }
+}
