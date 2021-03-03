2fe6c3b743bd480363a9781e78a205ccabf9863e
Richard Maness <rickrmaness@gmail.com>
2021-03-03 17:37:56 -0500

    added ZipCodeInfoClass

---
+++ zipCodeInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Postal_Code_Lookup
+{
+    class zipCodeInfo
+    {
+        public string CountryCode;
+        public int PostalCode;
+        public string PlaceName;
+        public string AdminNameOne;
+        public int AdminCodeOne;
+        public string AdminNameTwo;
+        public int AdminCodeTwo;
+        public string AdminNameThree;
+        public int AdminCodeThree;
+        public int Latitude;
+        public int Longitude;
+        public int Accuracy;
+
+
+        
+    }
+}
